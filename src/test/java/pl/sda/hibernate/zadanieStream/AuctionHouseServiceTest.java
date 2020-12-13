9cbd480f9893752ad178dced6529b3747996e3a2
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-12-13 11:56:13 +0100

    * AuctionHouseServiceTest.shouldConfirmInitialise * AuctionHouseServiceTest.shouldInitialiseData

--- src/test/java/pl/sda/hibernate/zadanieStream/AuctionHouseServiceTest.java
+++ src/test/java/pl/sda/hibernate/zadanieStream/AuctionHouseServiceTest.java
@@ -1,18 +1,32 @@
 package pl.sda.hibernate.zadanieStream;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.sda.hibernate.services.Screen;
 
 import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class AuctionHouseServiceTest {
+
+    private static final Room TEST_ROOM1 = new Room("Poznań", 16, 800, 800, "Warta");
+    private static final Room TEST_ROOM2 = new Room("Gdańsk", 15, 850, 1500, "Morze", "Plac zabaw");
+    private static final Room TEST_ROOM3 = new Room("Poznań", 13, 800, 2500, "Ulica", "Droga");
+
+    @Spy
+    final HashSet<Room> rooms = new HashSet<>();
 
     @Mock
     private Screen screen;
@@ -22,10 +36,27 @@ class AuctionHouseServiceTest {
 
     @BeforeEach
     void setUp() {
-        final HashSet<Room> rooms = new HashSet<>();
         auctionHouseService.rooms = rooms;
-        rooms.add(new Room("Poznań", 16, 800, 800, "Warta"));
-        rooms.add(new Room("Gdańsk", 15, 850, 1500, "Morze", "Plac zabaw"));
-        rooms.add(new Room("Poznań", 13, 800, 2500, "Ulica", "Droga"));
+        rooms.add(TEST_ROOM1);
+        rooms.add(TEST_ROOM2);
+        rooms.add(TEST_ROOM3);
+        reset(rooms);
+    }
+
+    @Test
+    void shouldConfirmInitialise() {
+        auctionHouseService.initialise();
+
+        verify(screen, times(1)).println(anyString());
+        verify(screen).println(contains("initialised"));
+        verify(screen).println("AuctionHouseService initialised");
+    }
+
+    @Test
+    void shouldInitialiseData() {
+
+        auctionHouseService.initialise();
+
+        verify(rooms, times(7)).add(any(Room.class));
     }
 }
